fe33fb77adc5efc9e7aec2f05a522bee9cea8f9d
Babi-B <babibyoolah@gmail.com>
2022-02-19 16:48:21 +0100

    can automatically detect window theme

--- app.js
+++ app.js
@@ -27,16 +27,11 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', eve
 
 theme.addEventListener("click", () => {
     if(theme.classList.contains("fa-moon")){
-        console.log(theme.classList)
+        window.matchMedia('(prefers-color-scheme: dark)');
         theme.classList.toggle("fa-sun");
         theme.classList.remove("fa-moon");
-        // document.documentElement.style.setProperty("--mainColor", "#181818")
-        // document.documentElement.style.setProperty("--secondaryColor", "#404040")
-        // document.documentElement.style.setProperty("--mainText", "#fff")
-        // document.documentElement.style.setProperty("--secondaryText", "rgb(233, 232, 232)")
     }
     else{
-        console.log(theme.classList)
         theme.classList.toggle("fa-moon");
         theme.classList.remove("fa-sun")
     }
